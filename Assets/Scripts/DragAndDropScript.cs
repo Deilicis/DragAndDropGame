d3a059e5fa0405d04da83339f5d7eb9f4ffd79de
Davis <daviszinkers@gmail.com>
2025-10-29 14:33:31 +0200

    added buttons for mobile

--- Assets/Scripts/DragAndDropScript.cs
+++ Assets/Scripts/DragAndDropScript.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragAndDropScript : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, 
+// CHANGES FOR ANDROID
+public class DragAndDropScript : MonoBehaviour, IPointerDownHandler, IBeginDragHandler,
     IDragHandler, IEndDragHandler
 {
     private CanvasGroup canvasGro;
@@ -15,103 +16,119 @@ public class DragAndDropScript : MonoBehaviour, IPointerDownHandler, IBeginDragH
     private Camera uiCamera;
     private Canvas canvas;
 
-    // Start is called before the first frame update
-    void Start()
+
+    void Awake()
     {
         canvasGro = GetComponent<CanvasGroup>();
         rectTra = GetComponent<RectTransform>();
-
-        objectScr = Object.FindFirstObjectByType<ObjectScript>();
-        screenBou = Object.FindFirstObjectByType<ScreenBoundriesScript>();
 
         if (objectScr == null)
         {
             objectScr = Object.FindFirstObjectByType<ObjectScript>();
         }
-        if (screenBou = null)
+
+        if (screenBou == null)
         {
-            screenBou = GetComponent<ScreenBoundriesScript>();
+            screenBou = Object.FindFirstObjectByType<ScreenBoundriesScript>();
         }
+
         canvas = GetComponentInParent<Canvas>();
         if (canvas != null)
         {
             uiCamera = canvas.worldCamera;
+
         }
-        else 
+        else
         {
-            Debug.LogError("Canvas not found - DragAndDropScript");
+            Debug.LogError("Canvas not found for DragAndDropScript");
         }
     }
-    //android changes
+
+
+
+    // CHANGES FOR ANDROID
     public void OnPointerDown(PointerEventData eventData)
     {
-            Debug.Log("OnPointerDown");
-            objectScr.effects.PlayOneShot(objectScr.audioCli[0]);
+        Debug.Log("OnPointerDown");
+        objectScr.effects.PlayOneShot(objectScr.audioCli[0]);
+
     }
-    //android changes
+
+    // CHANGES FOR ANDROID
     public void OnBeginDrag(PointerEventData eventData)
     {
-            ObjectScript.drag = true;
-            canvasGro.blocksRaycasts = false;
-            canvasGro.alpha = 0.6f;
-            //rectTra.SetAsLastSibling();
-            int lastIndex = transform.parent.childCount - 1;
-            int position = Mathf.Max(0, lastIndex - 1);
-            transform.SetSiblingIndex(position);
+        ObjectScript.drag = true;
+        ObjectScript.lastDragged = eventData.pointerDrag;
+        canvasGro.blocksRaycasts = false;
+        canvasGro.alpha = 0.6f;
+        //rectTra.SetAsLastSibling();
+        int lastIndex = transform.parent.childCount - 1;
+        int position = Mathf.Max(0, lastIndex - 1);
+        transform.SetSiblingIndex(position);
 
         Vector3 pointerWorld;
+
         if (ScreenPointToWorld(eventData.position, out pointerWorld))
         {
             dragOffsetWorld = transform.position - pointerWorld;
+
         }
-        else 
+        else
         {
             dragOffsetWorld = Vector3.zero;
         }
-        ObjectScript.lastDragged = eventData.pointerDrag;
 
-        
+        ObjectScript.lastDragged = eventData.pointerDrag;
     }
-    //changes for android
+
+
+    // CHANGES FOR ANDROID
     public void OnDrag(PointerEventData eventData)
     {
         Vector3 pointerWorld;
+
         if (!ScreenPointToWorld(eventData.position, out pointerWorld))
-        {
             return;
-        }
-        Vector3 desired = pointerWorld + dragOffsetWorld;
-        desired.z = rectTra.position.z;
+
+        Vector3 desiredPosition = pointerWorld + dragOffsetWorld;
+        desiredPosition.z = transform.position.z;
+
         screenBou.RecalculateBounds();
 
-        Vector2 clamped = screenBou.GetClampedPosition(desired);
-        transform.position = new Vector3(clamped.x, clamped.y, desired.z);
+        Vector2 clamped = screenBou.GetClampedPosition(desiredPosition);
+        transform.position = new Vector3(clamped.x, clamped.y, desiredPosition.z);
     }
-    //changes for yknow what
+
+    // CHANGES FOR ANDROID
     public void OnEndDrag(PointerEventData eventData)
     {
         objectScr.effects.PlayOneShot(objectScr.audioCli[0]);
-       
-            ObjectScript.drag = false;
-            canvasGro.blocksRaycasts = true;
-            canvasGro.alpha = 1.0f;
+        ObjectScript.drag = false;
+        canvasGro.blocksRaycasts = true;
+        canvasGro.alpha = 1.0f;
 
-            if(objectScr.rightPlace)
-            {
-               canvasGro.blocksRaycasts = false;
-                ObjectScript.lastDragged = null;
-            }
-            
-            objectScr.rightPlace = false;
+        if (objectScr.rightPlace)
+        {
+            canvasGro.blocksRaycasts = false;
+            ObjectScript.lastDragged = null;
+        }
+
+        objectScr.rightPlace = false;
     }
+
     private bool ScreenPointToWorld(Vector2 screenPoint, out Vector3 worldPoint)
     {
         worldPoint = Vector3.zero;
+
         if (uiCamera == null)
+        {
             return false;
+        }
 
         float z = Mathf.Abs(uiCamera.transform.position.z - transform.position.z);
         Vector3 sp = new Vector3(screenPoint.x, screenPoint.y, z);
+        worldPoint = uiCamera.ScreenToWorldPoint(sp);
+
         return true;
     }
 }
--- Assets/Scripts/TransformationScript.cs
+++ Assets/Scripts/TransformationScript.cs
@@ -1,50 +1,70 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.Rendering;
 
 public class TransformationScript : MonoBehaviour
 {
     public float rotationSpeed = 90f;
     public float scaleSpeed = 0.5f;
-
-    private bool rotateCW, rotateCCW, scaleUpY, scaleDownY, scaleUpX, scaleDownX;
     public static bool isTransforming = false;
+    private bool rotateCW, rotateCCW, scaleUpY, scaleDownY, scalUpX, scaleDownX;
+
 
 
     void Update()
     {
-        if (ObjectScript.lastDragged != null)
+        if (ObjectScript.lastDragged == null)
             return;
+
         RectTransform rt = ObjectScript.lastDragged.GetComponent<RectTransform>();
 
         if (rotateCW)
+        {
             rt.Rotate(0, 0, -rotationSpeed * Time.deltaTime);
-        if (rotateCCW)
-            rt.Rotate(0, 0, rotationSpeed * Time.deltaTime);
-        if (scaleUpY && rt.localScale.y < 0.8f)
-            rt.localScale += new Vector3(0, scaleSpeed * Time.deltaTime, 0);
-        if (scaleDownY && rt.localScale.y > 0.35f)
-            rt.localScale -= new Vector3(0, scaleSpeed * Time.deltaTime, 0);
-        if (scaleUpX && rt.localScale.x < 0.8f)
-            rt.localScale += new Vector3(scaleSpeed * Time.deltaTime, 0, 0);
-        if (scaleDownX && rt.localScale.x > 0.35f)
-            rt.localScale -= new Vector3(scaleSpeed * Time.deltaTime, 0, 0);
+        }
 
-        isTransforming = rotateCW || rotateCCW || scaleUpY || scaleDownY || scaleUpX || scaleDownX;
-        
+        if (rotateCCW)
+        {
+            rt.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+        }
+
+        if (scaleUpY && rt.localScale.y < 0.9f)
+        {
+            rt.localScale += new Vector3(0, scaleSpeed * Time.deltaTime, 0);
+        }
+
+        if (scaleDownY && rt.localScale.y > 0.35f)
+        {
+            rt.localScale -= new Vector3(0, scaleSpeed * Time.deltaTime, 0);
+        }
+
+        if (scalUpX && rt.localScale.x < 0.9f)
+        {
+            rt.localScale += new Vector3(scaleSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (scaleDownX && rt.localScale.x > 0.35f)
+        {
+            rt.localScale -= new Vector3(scaleSpeed * Time.deltaTime, 0, 0);
+        }
+
+        isTransforming = rotateCW || rotateCCW || scaleUpY || scaleDownY || scalUpX || scaleDownX;
     }
 
-    public void StartRotateCW(BaseEventData data){ rotateCW = true; }
+    public void StartRotateCW(BaseEventData data) { rotateCW = true; }
     public void StopRotateCW(BaseEventData data) { rotateCW = false; }
-    public void StartRotateCCW(BaseEventData data){ rotateCCW = true; }
+
+    public void StartRotateCCW(BaseEventData data) { rotateCCW = true; }
     public void StopRotateCCW(BaseEventData data) { rotateCCW = false; }
-    public void StartScaleUpX(BaseEventData data) { scaleUpX = true; }
-    public void StopScaleUpX(BaseEventData data) { scaleUpX = false; }
-    public void StartScaleDownX(BaseEventData data) { scaleDownX = true; }
-    public void StopScaleDownX(BaseEventData data) { scaleDownX = false; }
+
     public void StartScaleUpY(BaseEventData data) { scaleUpY = true; }
     public void StopScaleUpY(BaseEventData data) { scaleUpY = false; }
+
     public void StartScaleDownY(BaseEventData data) { scaleDownY = true; }
     public void StopScaleDownY(BaseEventData data) { scaleDownY = false; }
 
+    public void StartScaleUpX(BaseEventData data) { scalUpX = true; }
+    public void StopScaleUpX(BaseEventData data) { scalUpX = false; }
+
+    public void StartScaleDownX(BaseEventData data) { scaleDownX = true; }
+    public void StopScaleDownX(BaseEventData data) { scaleDownX = false; }
 }
